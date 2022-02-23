508f7015ab3e982648df24d13649bfbb321fd625
Mai.K <511014090@qq.com>
2022-02-23 15:07:27 +0800

    新增测试版固件每日新增记录

--- 三星测试版固件版本号解密.py
+++ 三星测试版固件版本号解密.py
@@ -1,4 +1,4 @@
-
+from sys import path
 import requests
 import hashlib
 from lxml import etree
@@ -10,19 +10,18 @@ from datetime import timedelta
 import json
 
 
-
 def getModelDicts():
     # 机型代号字典，格式为<设备代号":["固件前缀","系统版本前缀","地区代码"]>
     ModelDic = {
         "SM-N9760": ["N9760ZC", "N9760CHC", "CHC"],
         "SM-N9860": ["N9860ZC", "N9860OZL", "CHC"],
         "SM-T970": ["T970ZC", "T970CHN", "CHN"],
-        "SM-G9960":["G9960ZC","G9960CHC","CHC"],
-        "SM-G9860":["G9860ZC","G9860OZL","CHC"],
-        "SM-G9980":["G9980ZC","G9980CHC","CHC"],
-        "SM-S9010":["S9010ZC","S9010CHC","CHC"],
-        "SM-S9060":["S9060ZC","S9080CHC","CHC"],
-        "SM-S9080":["S9080ZC","S9080CHC","CHC"]
+        "SM-G9960": ["G9960ZC", "G9960CHC", "CHC"],
+        "SM-G9860": ["G9860ZC", "G9860OZL", "CHC"],
+        "SM-G9980": ["G9980ZC", "G9980CHC", "CHC"],
+        "SM-S9010": ["S9010ZC", "S9010CHC", "CHC"],
+        "SM-S9060": ["S9060ZC", "S9060CHC", "CHC"],
+        "SM-S9080": ["S9080ZC", "S9080CHC", "CHC"],
     }
     return ModelDic
 
@@ -37,7 +36,7 @@ def readXML(model):
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
         "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
-        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15"
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
     ]
     headers = {'User-Agent': random.choice(UA_list), 'Connection': 'close'}
     url = (
@@ -95,19 +94,38 @@ if __name__ == '__main__':
     modelDic = getModelDicts()
     jsonStr = ""
     SHA_TZ = timezone(
-    timedelta(hours=8),
-    name='Asia/Shanghai',
-)
-    now=datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(SHA_TZ).strftime('%Y-%m-%d %H:%M')
-    decDicts = {"update":now}
-    FilePath = './firmware.json'
-    if os.path.exists(FilePath):
-        os.remove(FilePath)
-        file = open(FilePath, 'w')
-    else:
-        file = open(FilePath, 'w')
-    for model in modelDic:
-        md5list = readXML(model)
-        decDicts.update(DecryptionFirmware(md5list, model))
-    file.write(json.dumps(decDicts))
-    file.close()
+        timedelta(hours=8),
+        name='Asia/Shanghai',
+    )
+    now = (
+        datetime.utcnow()
+        .replace(tzinfo=timezone.utc)
+        .astimezone(SHA_TZ)
+        .strftime('%Y-%m-%d %H:%M')
+    )
+    decDicts = {"update": now}
+    VerFilePath = 'firmware.json'
+    AddTxtPath = '测试版新增日志.txt'
+    verDic = {}
+    AddVer = {"update": now}
+    with open(VerFilePath, 'r') as f:
+        jsonStr = f.read()
+        oldJson = json.loads(jsonStr)
+        for model in modelDic:
+            md5list = readXML(model)
+            verDic = DecryptionFirmware(md5list, model)
+            decDicts.update(verDic)
+        file = open(AddTxtPath, 'a+')
+        file.write("*****记录时间:" + now + "*****\n")
+        for model in modelDic:
+            if (model in decDicts) & (model in oldJson):
+                md5Keys = decDicts[model].keys() - oldJson[model].keys()
+                if len(md5Keys) > 0:
+                    for md5Key in md5Keys:
+                        VerStr = str(decDicts[model][md5Key])
+                        file.write(
+                            model + "新增测试固件版本：" + VerStr + "，对应MD5值：" + md5Key + "\n"
+                        )
+        file.close()
+    with open(VerFilePath, 'w') as f:
+        f.write(json.dumps(decDicts))

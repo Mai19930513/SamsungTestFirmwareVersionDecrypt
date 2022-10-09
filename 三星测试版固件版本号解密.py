91a8dd5072a21c7b60ab2f50af1dd175c07ac3f1
Mai19930513 <511014090@qq.com>
2022-10-09 18:13:25 +0800

    更新

--- 三星测试版固件版本号解密.py
+++ 三星测试版固件版本号解密.py
@@ -1,5 +1,6 @@
 from genericpath import exists
 import time
+from xml.sax import make_parser
 import requests
 import hashlib
 from lxml import etree
@@ -26,16 +27,18 @@ def getModelDicts():
     # F:表示更新了6个版本，从A开始算
     # VG3:当前版本发布时间，V表示2022年(A表示2000),G表示7月(A表示1月),3表示第3个版本
     ModelDic = {
-        # 机型代号字典，格式为"设备代号:地区代码"
-        "SM-N9760": "CHC",
-        "SM-N9860": "CHC",
-        "SM-T970": "CHN",
-        "SM-G9960": "CHC",
-        "SM-G9860": "CHC",
-        "SM-G9980": "CHC",
-        "SM-S9010": "CHC",
-        "SM-S9060": "CHC",
-        "SM-S9080": "CHC"
+        # 机型代号字典，格式为"设备代号:{型号，名称}"
+        "SM-N9760": {'model': "CHC", 'name': 'Note 10+'},
+        "SM-N9860": {'model': "CHC", 'name': 'Note 20 Ultra'},
+        "SM-T970": {'model': "CHN", 'name': 'Tab S7+'},
+        "SM-G9810": {'model': "CHC", 'name': 'S20'},
+        "SM-G9860": {'model': "CHC", 'name': 'S20+'},
+        "SM-G9880": {'model': "CHC", 'name': 'S20 Ultra'},
+        "SM-G9960": {'model': "CHC", 'name': 'S21+'},
+        "SM-G9980": {'model': "CHC", 'name': 'S21 Ultra'},
+        "SM-S9010": {'model': "CHC", 'name': 'S22'},
+        "SM-S9060": {'model': "CHC", 'name': 'S22+'},
+        "SM-S9080": {'model': "CHC", 'name': 'S22 Ultra'}
     }
     return ModelDic
 
@@ -61,7 +64,7 @@ def readXML(model):
     md5list = []
     url = (
         "https://fota-cloud-dn.ospserver.net/firmware/"
-        + modelDic[model]
+        + modelDic[model]['model']
         + "/"
         + model
         + "/version.test.xml"
@@ -79,7 +82,7 @@ def DecryptionFirmware(md5list, model):
     modelDic = getModelDicts()
     url = (
         "https://fota-cloud-dn.ospserver.net/firmware/"
-        + modelDic[model]
+        + modelDic[model]['model']
         + "/"
         + model
         + "/version.xml"
@@ -90,12 +93,16 @@ def DecryptionFirmware(md5list, model):
     ApCode = latestVer[0][:-6]  # 如：N9860ZC
     CscCode = latestVer[1][:-5]  # 如：N9860OZL
     Dicts = {}
+    Dicts[model] = {}
+    Dicts[model]['versions'] = {}
+    Dicts[model]['latestVersion'] = ''
     DecDicts = {}
     tempCpVersions = []
     VerFilePath = 'firmware.json'
     lastVersion = ''
     startUpdateCount = 65  # 设置版本号中更新次数为A，即1次
     startYear = 65  # 设置版本号中更新年份为A，即2000年
+    startMonth = 65   # 设置版本号中更新月份为A，即2000年
     startJJ = 1
     with open(VerFilePath, 'r', encoding='utf-8') as f:
         jsonStr = f.read()
@@ -108,6 +115,7 @@ def DecryptionFirmware(md5list, model):
         startJJ = int(lastVersion[-5])
         startUpdateCount = ord(lastVersion[-4])
         startYear = ord(lastVersion[-3])
+        startMonth = ord(lastVersion[-2])
     starttime = time.perf_counter()
     for i1 in "US":
         jjNumber = int(latestVer[0][-5])+1
@@ -116,40 +124,42 @@ def DecryptionFirmware(md5list, model):
             for k1 in range(startUpdateCount, updateCount):
                 curYear = ord(latestVer[0][-3])+1  # 获取当前年份，,倒数第3位
                 for l1 in range(startYear, curYear):  # A表示2000年，后面递增
-                    for m1 in range(65, 77):  # A-L表示1-12月
+                    for m1 in range(startMonth, 77):  # A-L表示1-12月
                         for n1 in "123456789ABCDEFGHIJK":
                             vc = str(j1) + chr(k1) + \
                                 chr(l1) + chr(m1) + n1  # 版本号
                             CpCode = ''if latestVer[2] == '' else ApCode + i1 + vc
                             version1 = ApCode + i1 + vc + "/" + CscCode + vc + "/"+CpCode
+                            if(model in oldJson.keys() and 'versions' in oldJson[model].keys()) and version1 in oldJson[model]['versions'].values():
+                                continue
                             md5 = hashlib.md5()
                             md5.update(version1.encode(encoding="utf-8"))
+                            # 基带和固件版本一致时
                             if md5.hexdigest() in md5list:
                                 DecDicts[md5.hexdigest()] = version1
                                 if(version1.split('/')[2] != ''):
                                     tempCpVersions.append(
                                         version1.split('/')[2])
+                            # 基带和固件版本不一致时
                             if(latestVer[2] != '' and tempCpVersions != ''):
-                                for tempCpVersion in tempCpVersions:
+                                for tempCpVersion in tempCpVersions[-6:]:
                                     version2 = ApCode + i1 + vc + "/" + CscCode + vc + "/"+tempCpVersion
                                     md5 = hashlib.md5()
                                     md5.update(version2.encode(
                                         encoding="utf-8"))
                                     if md5.hexdigest() in md5list:
                                         DecDicts[md5.hexdigest()] = version2
-
-    def cmp(a, b):
-        if a.split('/')[0][-4:] > b.split('/')[0][-4:]:
-            return 1
-        else:
-            return -1
-    DecDicts['latestVersion'] = sorted(
-        DecDicts.values(), key=cmp_to_key(cmp))[-1]  # 记录最新版本号
+    if len(DecDicts.values()) > 0:
+        Dicts[model]['latestVersion'] = sorted(
+            DecDicts.values(), key=lambda x: x.split('/')[0][-4:])[-1]  # 记录最新版本号
+        Dicts[model]['versions'] = DecDicts
     endtime = time.perf_counter()
     print("机型：" + model + " 测试版固件版本号解密完毕," +
           '耗时:', round(endtime - starttime, 2), '秒')
-    print(model+' 此次解密'+str(len(DecDicts)-1)+'个测试版本号')
-    Dicts[model] = DecDicts
+    if len(DecDicts)>0: 
+        print(model+' 此次解密'+str(len(DecDicts))+'个测试版本号')
+    else:
+        print(model+' 无新增!')
     return Dicts
 
 # 使用TG机器人推送
@@ -188,6 +198,7 @@ def fcm(title: str, content: str, link: str) -> None:
 
 
 push_config = {
+    'FCM_API_KEY': '',
     'FCM_KEY': '',
     'PUSH_KEY': '',                     # server 酱的 PUSH_KEY，兼容旧版与 Turbo 版
     # 必填 tg 机器人的 TG_BOT_TOKEN，例：1407203283:AAG9rt-6RDaaX0HBLZQq0laNOh898iFYaRQ
@@ -218,7 +229,7 @@ if __name__ == '__main__':
         .astimezone(SHA_TZ)
         .strftime('%Y-%m-%d %H:%M')
     )
-    decDicts = {"update": now}
+    decDicts = {"上次更新时间": now}
     VerFilePath = 'firmware.json'
     AddTxtPath = '测试版新增日志.txt'
     verDic = {}
@@ -231,42 +242,59 @@ if __name__ == '__main__':
         for model in modelDic:
             md5list = readXML(model)
             newMDic = {}
-            newMDic[model] = deepcopy(oldJson[model])
-            decDicts.update(newMDic)   #先保存已有的数据
-            verDic = DecryptionFirmware(md5list, model) #解密获取新数据
-            for item in verDic[model]:
-                if not item in newMDic[model].keys():
-                    hasNewVersion = True
-                    newMDic[model][item] = verDic[model][item] #存入新的版本号
+            if model in oldJson.keys():
+                newMDic[model] = deepcopy(oldJson[model])
+            else:
+                newMDic[model] = {}
+                newMDic[model]['versions'] = {}
+                newMDic[model]['latestVersion'] = ''
+            decDicts.update(newMDic)  # 先保存已有的数据
+            verDic = DecryptionFirmware(md5list, model)  # 解密获取新数据
+            diffModel = verDic[model]['versions'].keys()
+            if verDic[model]['latestVersion'] != '':
+                newMDic[model]['latestVersion'] = verDic[model]['latestVersion']
+            if len(newMDic[model]['versions'].keys()) > 0:
+                diffModel = verDic[model]['versions'].keys(
+                )-newMDic[model]['versions'].keys()
+
+            if len(diffModel) > 0:
+                hasNewVersion = True
+                for key in diffModel:
+                    # 存入新的版本号
+                    newMDic[model]['versions'][key] = verDic[model]['versions'][key]
+        newMDic
         decDicts.update(newMDic)
         if hasNewVersion:
+            # 固件更新日志
             with open(AddTxtPath, 'a+', encoding='utf-8') as file:
                 file.write("*****记录时间:" + now + "*****\n")
                 for model in modelDic:
-                    if (model in decDicts) & (model in oldJson):
-                        md5Keys = decDicts[model].keys(
-                        ) - oldJson[model].keys()  # 获取新增的版本号
+                    if (model in decDicts) and (model in oldJson):
+                        md5Keys = decDicts[model]['versions'].keys(
+                        ) - oldJson[model]['versions'].keys()  # 获取新增的版本号
                         if len(md5Keys) > 0:
                             textStr = ''
                             Str = ''
                             for md5Key in md5Keys:
-                                VerStr = str(decDicts[model][md5Key])
-                                textStr += VerStr + "\n"
-                                if(md5Key == 'latestVersion'):
-                                    continue
-                                Str += model + "新增测试固件版本：" + VerStr + "，对应MD5值：" + md5Key + "\n"
+                                VerStr = str(
+                                    decDicts[model]['versions'][md5Key])
+                                textStr += "\n"+VerStr
+                                Str += modelDic[model]['name'] + "新增测试固件版本：" + \
+                                    VerStr + "，对应MD5值：" + md5Key + "\n"
                             file.write(Str)
-                            fcm(model+"新增内测固件", textStr.replace('*', ''), '')
-                            telegram_bot('#'+model.split('-')
-                                         [1]+"新增内测固件", textStr)
+                            fcm(modelDic[model]['name']+"新增内测固件",
+                                textStr.replace('*', ''), '')
+                            telegram_bot('#'+modelDic[model]['name']
+                                         + "新增内测固件", textStr)
+            # 更新全机型最新版
             with open('各机型最新版本.txt', 'w', encoding='utf-8') as f:
                 modelDic = getModelDicts()
                 textStr = ''
                 for model in modelDic.keys():
-                    textStr += model+':' + \
+                    textStr += "**"+modelDic[model]['name']+"：↓**\n" + \
                         decDicts[model]['latestVersion']+'\n'
                 f.write(textStr.replace('*', ''))
                 fcm("各机型最新测试版", textStr.replace('*', ''), '')
-                telegram_bot("#各机型最新测试版\n\n", textStr)
+                telegram_bot("#各机型最新测试版\n", textStr)
     with open(VerFilePath, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(decDicts))
+        f.write(json.dumps(decDicts, indent=4, ensure_ascii=False))

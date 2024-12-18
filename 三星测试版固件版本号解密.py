f1a454563bb6abc9c1c32240f19faff46146875e
Mai1993 <511014090@qq.com>
2024-12-19 01:34:57 +0800

    Update 三星测试版固件版本号解密.py

--- 三星测试版固件版本号解密.py
+++ 三星测试版固件版本号解密.py
@@ -271,6 +271,7 @@ def get_pre_char(char, alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
     else:
         raise ValueError(f"字符 '{char}' 不在给定的字符串中")
 
+@func_set_timeout(2000)
 def DecryptionFirmware(model:str, md5Dic:dict, cc:str)->dict:
     '''通过穷举解码固件号
     Args:
@@ -528,7 +529,7 @@ push_config = {
 }
 
 
-@func_set_timeout(2000)
+
 def run():
     # 获取相关参数变量数据
     for k in push_config:
